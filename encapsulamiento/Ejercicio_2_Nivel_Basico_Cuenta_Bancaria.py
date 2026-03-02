7a6c2b66ce988de32a4f5751f92cf09c6f146356
Gerson Jara <46004475+Gersonjaragarcia@users.noreply.github.comQ>
2026-03-01 22:48:27 -0500

    ejercicio 2 terminado

--- encapsulamiento/Ejercicio_2_Nivel_Basico_Cuenta_Bancaria.py
+++ encapsulamiento/Ejercicio_2_Nivel_Basico_Cuenta_Bancaria.py
@@ -6,3 +6,35 @@ Crea una clase Cuenta que tenga un atributo privado __saldo.
     Método Extra: retirar(monto) que reste al saldo solo si hay fondos suficientes. Si no, imprime un error.
     Objetivo: Practicar la lógica de control dentro de los métodos.
 """
+class Cuenta:
+    def __init__(self, saldo_inicial):
+        self.__saldo = saldo_inicial # Atributo privado
+    
+    # Getter para consultar el saldo
+    def get_saldo(self):
+        return self.__saldo
+    
+    # Método para depositar (actúa como un setter especial)
+    def depositar(self, monto):
+        if monto > 0:
+            self.__saldo += monto
+            print(f"Depósito exitoso. Nuevo saldo: {self.__saldo}")
+        else:
+            print("El monto a depositar debe ser positivo.")
+
+    # Método para retirar
+    def retirar(self, monto):
+        if 0 < monto <= self.__saldo:
+            self.__saldo -= monto
+            print(f"Retiro exitoso. Saldo restante: {self.__saldo}")
+        else:
+            print("Fondos insuficientes o monto inválido.")
+
+# --- Prueba del código ---
+mi_cuenta = Cuenta(1000)
+print(f"Saldo inicial: {mi_cuenta.get_saldo()}")
+
+mi_cuenta.depositar(500)
+mi_cuenta.retirar(200)
+
+
