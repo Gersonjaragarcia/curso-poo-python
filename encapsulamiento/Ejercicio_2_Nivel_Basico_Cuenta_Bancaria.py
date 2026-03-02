df8c37e798f2e0575e8970fe144c96928b1676d9
Gerson Jara <46004475+Gersonjaragarcia@users.noreply.github.comQ>
2026-03-01 22:29:48 -0500

    Segundo ejercicio

---
+++ encapsulamiento/Ejercicio_2_Nivel_Basico_Cuenta_Bancaria.py
@@ -0,0 +1,8 @@
+"""
+Crea una clase Cuenta que tenga un atributo privado __saldo.
+
+    Getter: get_saldo().
+    Setter: depositar(monto) (solo si el monto es mayor a 0).
+    Método Extra: retirar(monto) que reste al saldo solo si hay fondos suficientes. Si no, imprime un error.
+    Objetivo: Practicar la lógica de control dentro de los métodos.
+"""
